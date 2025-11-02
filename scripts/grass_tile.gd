8bed38c3ff9cc5fcff8e2ebc86025fbd5fca5a66
NoNormalDev <d.jochymek12@gmail.com>
2025-11-02 16:13:50 +0100

    progress

--- scripts/grass_tile.gd
+++ scripts/grass_tile.gd
@@ -1,3 +1,24 @@
+@tool
 extends Node2D
 
-@export var dark: bool = false
+@export var dark: bool:
+	set(x):
+		dark = x	
+		update_material()
+
+var api_hovered: bool = false
+signal api_pressed
+
+func _button_pressed() -> void:
+	api_pressed.emit()
+	
+@onready var _btn: Button = $Button
+func _process(_delta: float) -> void:
+	api_hovered = _btn.is_hovered()
+
+func update_material() -> void:
+	$MeshInstance2D.material.set_shader_parameter("bright", 0.75 if dark else 1.0)
+
+func _ready() -> void:
+	$MeshInstance2D.material = $MeshInstance2D.material.duplicate()
+	update_material()
