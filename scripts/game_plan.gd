c1e4e0a555773b9574590843a38845e7191432ca
NoNormalDev <d.jochymek12@gmail.com>
2025-11-02 12:19:44 +0100

    progress

--- scripts/game_plan.gd
+++ scripts/game_plan.gd
@@ -1,5 +1,16 @@
 extends Node2D
 
+@onready var _node_borders: ColorRect = $Borders
+@onready var _node_game: Node2D = $Game
 func update_window_borders() -> void:
 	var window_size: Vector2 = get_viewport_rect().size
+	_node_borders.size = window_size
 	var aspect_ratio: float = window_size.x / window_size.y
+	if aspect_ratio == 16/9:
+		_node_game.position = Vector2(0.0,0.0)
+		var game_scale: float = window_size.x / 1080.0
+		_node_game.scale.x = game_scale
+		_node_game.scale.y = game_scale
+
+func _process(_delta: float) -> void:
+	update_window_borders()
